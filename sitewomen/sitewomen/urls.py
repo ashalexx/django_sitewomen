144f3142423b8333565dd47b0f114722d17dbe9e
Alexey <ashalexx@gmail.com>
2025-07-24 10:45:30 +0700

    Начало работы - добавил .env

--- sitewomen/sitewomen/urls.py
+++ sitewomen/sitewomen/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from women import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('women/', views.index),
-    path('cats/', views.categories),
+    path('women/', include('women.urls')),
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -8,5 +8,9 @@ def index(request):
     return HttpResponse("Страница приложения women.")
 
 
-def categories(request):
-    return HttpResponse("<h1>Статьи по категориям</h1>")
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id:{cat_id}</p>")
+
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug:{cat_slug}</p>")
