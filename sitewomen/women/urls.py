38b4eb0f34d946f0b185c5d353cd0cb8fdf66b92
Alexey <ashalexx@gmail.com>
2025-07-24 11:43:34 +0700

    Работа 1 - не отслеживался requirements.txt и urls.py приложения women

---
+++ sitewomen/women/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from women import views
+
+urlpatterns = [
+    path('', views.index),
+    path('cats/<int:cat_id>/', views.categories),
+    path('cats/<slug:cat_slug>/', views.categories_by_slug),
+]
